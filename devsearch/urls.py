ce317c7898090e951015cef734e7ac5ec8427c16
Chris Linton <chris.linton@mail.com>
2022-03-25 08:56:50 -0700

    password reset

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -17,13 +17,34 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+# django authentication system/ auth views, we're using for pw reset
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+
+    # built-in class based views for pw reset see comments at bottom rendered w/ .as_view() method; naming has to match django documentation; django provides built-in templates; if want to customize must create own and enter template_name as attribute to view call:
+    # link to pw reset page
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'),
+         name="reset_password"),
+    # confirmation that pw reset email sent
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'),
+         name="password_reset_done"),
+    # link in pw reset instructions email that was sent, <uidb64> encodes user id in base 64 encryption and token is passed in
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'),
+         name="password_reset_confirm"),
+    # pw reset success message
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'),
+         name="password_reset_complete"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# 1 - User submits email for reset              //PasswordResetView.as_view()           //name="reset_password"
+# 2 - Email sent message                        //PasswordResetDoneView.as_view()        //name="passsword_reset_done"
+# 3 - Email with link and reset instructions    //PasswordResetConfirmView()            //name="password_reset_confirm"
+# 4 - Password successfully reset message       //PasswordResetCompleteView.as_view()   //name="password_reset_complete"
